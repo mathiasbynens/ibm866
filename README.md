810517939043e0c7c9025914c6f29558ea076f92
Mathias Bynens <mathias@qiwi.be>
2014-05-06 15:09:11 +0200

    Initial release

---
+++ README.md
@@ -0,0 +1,123 @@
+# ibm866 [![Build status](https://travis-ci.org/mathiasbynens/ibm866.svg?branch=master)](https://travis-ci.org/mathiasbynens/ibm866) [![Dependency status](https://gemnasium.com/mathiasbynens/ibm866.svg)](https://gemnasium.com/mathiasbynens/ibm866)
+
+_ibm866_ is robust JavaScript implementation of [the ibm866 character encoding as defined by the Encoding Standard](http://encoding.spec.whatwg.org/#ibm866).
+
+This encoding is known under the following names: 866, cp866, csibm866, ibm866, and ibm866.
+
+## Installation
+
+Via [npm](http://npmjs.org/):
+
+```bash
+npm install ibm866
+```
+
+Via [Bower](http://bower.io/):
+
+```bash
+bower install ibm866
+```
+
+Via [Component](https://github.com/component/component):
+
+```bash
+component install mathiasbynens/ibm866
+```
+
+In a browser:
+
+```html
+<script src="ibm866.js"></script>
+```
+
+In [Narwhal](http://narwhaljs.org/), [Node.js](http://nodejs.org/), and [RingoJS](http://ringojs.org/):
+
+```js
+var ibm866 = require('ibm866');
+```
+
+In [Rhino](http://www.mozilla.org/rhino/):
+
+```js
+load('ibm866.js');
+```
+
+Using an AMD loader like [RequireJS](http://requirejs.org/):
+
+```js
+require(
+  {
+    'paths': {
+      'ibm866': 'path/to/ibm866'
+    }
+  },
+  ['ibm866'],
+  function(ibm866) {
+    console.log(ibm866);
+  }
+);
+```
+
+## API
+
+### `ibm866.version`
+
+A string representing the semantic version number.
+
+### `ibm866.encode(input, options)`
+
+This function takes a plain text string (the `input` parameter) and encodes it according to ibm866. The return value is a ‘byte string’, i.e. a string of which each item represents an octet as per ibm866.
+
+```js
+var encodedData = ibm866.encode(text);
+```
+
+The optional `options` object and its `mode` property can be used to set the [error mode](http://encoding.spec.whatwg.org/#error-mode). For encoding, the error mode can be `'fatal'` (the default) or `'html'`.
+
+```js
+var encodedData = ibm866.encode(text, {
+  'mode': 'html'
+});
+// If `text` contains a symbol that cannot be represented in ibm866,
+// instead of throwing an error, it will return an HTML entity for the symbol.
+```
+
+### `ibm866.decode(input, options)`
+
+This function takes a byte string (the `input` parameter) and decodes it according to ibm866.
+
+```js
+var text = ibm866.decode(encodedData);
+```
+
+The optional `options` object and its `mode` property can be used to set the [error mode](http://encoding.spec.whatwg.org/#error-mode). For decoding, the error mode can be `'replacement'` (the default) or `'fatal'`.
+
+```js
+var text = ibm866.decode(encodedData, {
+  'mode': 'fatal'
+});
+// If `encodedData` contains an invalid byte for the ibm866 encoding,
+// instead of replacing it with U+FFFD in the output, an error is thrown.
+```
+
+## Support
+
+_ibm866_ is designed to work in at least Node.js v0.10.0, Narwhal 0.3.2, RingoJS 0.8-0.9, PhantomJS 1.9.0, Rhino 1.7RC4, as well as old and modern versions of Chrome, Firefox, Safari, Opera, and Internet Explorer.
+
+## Unit tests & code coverage
+
+After cloning this repository, run `npm install` to install the dependencies needed for development and testing. You may want to install Istanbul _globally_ using `npm install istanbul -g`.
+
+Once that’s done, you can run the unit tests in Node using `npm test` or `node tests/tests.js`. To run the tests in Rhino, Ringo, Narwhal, and web browsers as well, use `grunt test`.
+
+To generate the code coverage report, use `grunt cover`.
+
+## Author
+
+| [![twitter/mathias](https://gravatar.com/avatar/24e08a9ea84deb17ae121074d0f17125?s=70)](https://twitter.com/mathias "Follow @mathias on Twitter") |
+|---|
+| [Mathias Bynens](http://mathiasbynens.be/) |
+
+## License
+
+_ibm866_ is available under the [MIT](http://mths.be/mit) license.
---
+++ tests/tests.js
@@ -0,0 +1,158 @@
+(function(root) {
+	'use strict';
+
+	var noop = Function.prototype;
+
+	var load = (typeof require == 'function' && !(root.define && define.amd)) ?
+		require :
+		(!root.document && root.java && root.load) || noop;
+
+	var QUnit = (function() {
+		return root.QUnit || (
+			root.addEventListener || (root.addEventListener = noop),
+			root.setTimeout || (root.setTimeout = noop),
+			root.QUnit = load('../node_modules/qunitjs/qunit/qunit.js') || root.QUnit,
+			addEventListener === noop && delete root.addEventListener,
+			root.QUnit
+		);
+	}());
+
+	var qe = load('../node_modules/qunit-extras/qunit-extras.js');
+	if (qe) {
+		qe.runInContext(root);
+	}
+
+	// The `ibm866` object to test
+	var ibm866 = root.ibm866 || (root.ibm866 = (
+		ibm866 = load('../ibm866.js') || root.ibm866,
+		ibm866 = ibm866.ibm866 || ibm866
+	));
+
+	/*--------------------------------------------------------------------------*/
+
+	// `throws` is a reserved word in ES3; alias it to avoid errors
+	var raises = QUnit.assert['throws'];
+
+	// explicitly call `QUnit.module()` instead of `module()`
+	// in case we are in a CLI environment
+	QUnit.module('ibm866');
+
+	test('ibm866.encode', function() {
+		equal(
+			ibm866.encode('\0\x01\x02\x03\x04\x05\x06\x07\b\t\n\x0B\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7F'),
+			'\0\x01\x02\x03\x04\x05\x06\x07\b\t\n\x0B\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7F',
+			'U+0000 to U+007F remain unchanged'
+		);
+		equal(
+			ibm866.encode('\u0410\u0411\u0412\u0413\u0414\u0415\u0416\u0417\u0418\u0419\u041A\u041B\u041C\u041D\u041E\u041F\u0420\u0421\u0422\u0423\u0424\u0425\u0426\u0427\u0428\u0429\u042A\u042B\u042C\u042D\u042E\u042F\u0430\u0431\u0432\u0433\u0434\u0435\u0436\u0437\u0438\u0439\u043A\u043B\u043C\u043D\u043E\u043F\u2591\u2592\u2593\u2502\u2524\u2561\u2562\u2556\u2555\u2563\u2551\u2557\u255D\u255C\u255B\u2510\u2514\u2534\u252C\u251C\u2500\u253C\u255E\u255F\u255A\u2554\u2569\u2566\u2560\u2550\u256C\u2567\u2568\u2564\u2565\u2559\u2558\u2552\u2553\u256B\u256A\u2518\u250C\u2588\u2584\u258C\u2590\u2580\u0440\u0441\u0442\u0443\u0444\u0445\u0446\u0447\u0448\u0449\u044A\u044B\u044C\u044D\u044E\u044F\u0401\u0451\u0404\u0454\u0407\u0457\u040E\u045E\xB0\u2219\xB7\u221A\u2116\xA4\u25A0\xA0'),
+			'\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8A\x8B\x8C\x8D\x8E\x8F\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9A\x9B\x9C\x9D\x9E\x9F\xA0\xA1\xA2\xA3\xA4\xA5\xA6\xA7\xA8\xA9\xAA\xAB\xAC\xAD\xAE\xAF\xB0\xB1\xB2\xB3\xB4\xB5\xB6\xB7\xB8\xB9\xBA\xBB\xBC\xBD\xBE\xBF\xC0\xC1\xC2\xC3\xC4\xC5\xC6\xC7\xC8\xC9\xCA\xCB\xCC\xCD\xCE\xCF\xD0\xD1\xD2\xD3\xD4\xD5\xD6\xD7\xD8\xD9\xDA\xDB\xDC\xDD\xDE\xDF\xE0\xE1\xE2\xE3\xE4\xE5\xE6\xE7\xE8\xE9\xEA\xEB\xEC\xED\xEE\xEF\xF0\xF1\xF2\xF3\xF4\xF5\xF6\xF7\xF8\xF9\xFA\xFB\xFC\xFD\xFE\xFF',
+			'Encoding all other symbols in the character set'
+		);
+		raises(
+			function() {
+				ibm866.encode('\uFFFF');
+			},
+			Error,
+			'Encoding a code point that is invalid for this encoding throws an error in `fatal` mode, which is the implied default for `encode()`'
+		);
+		raises(
+			function() {
+				ibm866.encode('\uFFFF', { 'mode': 'fatal' });
+			},
+			Error,
+			'Encoding a code point that is invalid for this encoding throws an error in `fatal` mode'
+		);
+		raises(
+			function() {
+				ibm866.encode('\uFFFF', { 'mode': 'FATAL' });
+			},
+			Error,
+			'Mode names are case-insensitive'
+		);
+		raises(
+			function() {
+				ibm866.encode('\uFFFF', { 'mode': 'fAtAl' });
+			},
+			Error,
+			'Mode names are case-insensitive'
+		);
+		equal(
+			ibm866.encode('\uFFFF', { 'mode': 'html' }),
+			'&#65535;',
+			'Encoding a code point that is invalid for this encoding returns an HTML entity in `html` mode'
+		);
+		equal(
+			ibm866.encode('\uFFFF', { 'mode': 'HTML' }),
+			'&#65535;',
+			'Mode names are case-insensitive'
+		);
+		equal(
+			ibm866.encode('\uFFFF', { 'mode': 'hTmL' }),
+			'&#65535;',
+			'Mode names are case-insensitive'
+		);
+	});
+
+	test('ibm866.decode', function() {
+		equal(
+			ibm866.decode('\0\x01\x02\x03\x04\x05\x06\x07\b\t\n\x0B\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7F'),
+			'\0\x01\x02\x03\x04\x05\x06\x07\b\t\n\x0B\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7F',
+			'U+0000 to U+007F remain unchanged'
+		);
+		equal(
+			ibm866.decode('\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8A\x8B\x8C\x8D\x8E\x8F\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9A\x9B\x9C\x9D\x9E\x9F\xA0\xA1\xA2\xA3\xA4\xA5\xA6\xA7\xA8\xA9\xAA\xAB\xAC\xAD\xAE\xAF\xB0\xB1\xB2\xB3\xB4\xB5\xB6\xB7\xB8\xB9\xBA\xBB\xBC\xBD\xBE\xBF\xC0\xC1\xC2\xC3\xC4\xC5\xC6\xC7\xC8\xC9\xCA\xCB\xCC\xCD\xCE\xCF\xD0\xD1\xD2\xD3\xD4\xD5\xD6\xD7\xD8\xD9\xDA\xDB\xDC\xDD\xDE\xDF\xE0\xE1\xE2\xE3\xE4\xE5\xE6\xE7\xE8\xE9\xEA\xEB\xEC\xED\xEE\xEF\xF0\xF1\xF2\xF3\xF4\xF5\xF6\xF7\xF8\xF9\xFA\xFB\xFC\xFD\xFE\xFF'),
+			'\u0410\u0411\u0412\u0413\u0414\u0415\u0416\u0417\u0418\u0419\u041A\u041B\u041C\u041D\u041E\u041F\u0420\u0421\u0422\u0423\u0424\u0425\u0426\u0427\u0428\u0429\u042A\u042B\u042C\u042D\u042E\u042F\u0430\u0431\u0432\u0433\u0434\u0435\u0436\u0437\u0438\u0439\u043A\u043B\u043C\u043D\u043E\u043F\u2591\u2592\u2593\u2502\u2524\u2561\u2562\u2556\u2555\u2563\u2551\u2557\u255D\u255C\u255B\u2510\u2514\u2534\u252C\u251C\u2500\u253C\u255E\u255F\u255A\u2554\u2569\u2566\u2560\u2550\u256C\u2567\u2568\u2564\u2565\u2559\u2558\u2552\u2553\u256B\u256A\u2518\u250C\u2588\u2584\u258C\u2590\u2580\u0440\u0441\u0442\u0443\u0444\u0445\u0446\u0447\u0448\u0449\u044A\u044B\u044C\u044D\u044E\u044F\u0401\u0451\u0404\u0454\u0407\u0457\u040E\u045E\xB0\u2219\xB7\u221A\u2116\xA4\u25A0\xA0',
+			'Decoding all other symbols in the character set'
+		);
+		equal(
+			ibm866.decode('\uFFFF'),
+			'\uFFFD',
+			'Decoding a byte that is invalid for this encoding returns U+FFFD in `replacement` mode, which is the implied default for `decode()`'
+		);
+		equal(
+			ibm866.decode('\uFFFF', { 'mode': 'replacement' }),
+			'\uFFFD',
+			'Decoding a byte that is invalid for this encoding returns U+FFFD in `replacement` mode'
+		);
+		equal(
+			ibm866.decode('\uFFFF', { 'mode': 'REPLACEMENT' }),
+			'\uFFFD',
+			'Mode names are case-insensitive'
+		);
+		equal(
+			ibm866.decode('\uFFFF', { 'mode': 'rEpLaCeMeNt' }),
+			'\uFFFD',
+			'Mode names are case-insensitive'
+		);
+		raises(
+			function() {
+				ibm866.decode('\uFFFF', { 'mode': 'fatal' });
+			},
+			Error,
+			'Decoding a byte that is invalid for this encoding throws an error in `fatal` mode'
+		);
+		raises(
+			function() {
+				ibm866.decode('\uFFFF', { 'mode': 'FATAL' });
+			},
+			Error,
+			'Decoding a byte that is invalid for this encoding throws an error in `fatal` mode'
+		);
+		raises(
+			function() {
+				ibm866.decode('\uFFFF', { 'mode': 'fAtAl' });
+			},
+			Error,
+			'Mode names are case-insensitive'
+		);
+	});
+
+	/*--------------------------------------------------------------------------*/
+
+	// configure QUnit and call `QUnit.start()` for
+	// Narwhal, Node.js, PhantomJS, Rhino, and RingoJS
+	if (!root.document || root.phantom) {
+		QUnit.config.noglobals = true;
+		QUnit.start();
+	}
+}(typeof global == 'object' && global || this));
