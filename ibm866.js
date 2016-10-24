5d32917c898cd60ced457ce0bc42d14a9e628891
Mathias Bynens <mathias@qiwi.be>
2016-10-24 19:26:04 +0200

    Release v1.0.0

--- ibm866.js
+++ ibm866.js
@@ -1,4 +1,4 @@
-/*! https://mths.be/ibm866 v0.1.2 by @mathias | MIT license */
+/*! https://mths.be/ibm866 v1.0.0 by @mathias | MIT license */
 ;(function(root) {
 
 	// Detect free variables `exports`.
@@ -121,7 +121,7 @@
 			'csibm866',
 			'ibm866'
 		],
-		'version': '0.1.2'
+		'version': '1.0.0'
 	};
 
 	// Some AMD build optimizers, like r.js, check for specific condition patterns
