b498d9c3e21021fe5ecb612e7294fc9affb09756
Mathias Bynens <mathias@qiwi.be>
2021-07-30 12:22:33 +0200

    Release v1.0.2

--- ibm866.js
+++ ibm866.js
@@ -1,4 +1,4 @@
-/*! https://mths.be/ibm866 v1.0.1 by @mathias | MIT license */
+/*! https://mths.be/ibm866 v1.0.2 by @mathias | MIT license */
 ;(function() {
 
 	var object = {};
@@ -105,7 +105,7 @@
 			'csibm866',
 			'ibm866'
 		],
-		version: '1.0.1',
+		version: '1.0.2',
 	};
 
 	module.exports = ibm866;
